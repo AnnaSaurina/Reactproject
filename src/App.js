5cb00b79ffeec98ea47a3826154231eb281c0683
AnnaSaurina <the_happy_ann@mail.ru>
2023-09-01 01:07:45 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { Chart } from "react-google-charts";
 import './App.css';
+
+export const data = [
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+
+export const options = { 
+  chart: {
+    title: "Company Performance",
+    subtitle: "Sales, Expenses, and Profit: 2014-2017",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Chart
+      chartType="ScatterChart"
+      data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+      width="100%"
+      height="400px"
+      legendToggle
+      />
+    </>
   );
 }
 
