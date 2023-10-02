65c7aad805df59e57247ab2f0bec32c88ca74426
AnnaSaurina <the_happy_ann@mail.ru>
2023-10-02 14:23:04 +0300

    changes to const data

--- src/App.js
+++ src/App.js
@@ -2,31 +2,28 @@ import { Chart } from "react-google-charts";
 import './App.css';
 
 export const data = [
-  ["Year", "Sales", "Expenses", "Profit"],
-  ["2014", 1000, 400, 200],
-  ["2015", 1170, 460, 250],
-  ["2016", 660, 1120, 300],
-  ["2017", 1030, 540, 350],
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
 ];
 
-export const options = { 
-  chart: {
-    title: "Company Performance",
-    subtitle: "Sales, Expenses, and Profit: 2014-2017",
-  },
+export const options = {
+  title: "My Daily Activities",
+  is3D: true,
 };
 
-function App() {
+export function App() {
   return (
-    <>
-      <Chart
-      chartType="ScatterChart"
-      data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-      width="100%"
-      height="400px"
-      legendToggle
-      />
-    </>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
